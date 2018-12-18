c6f0e9eac4c9102ff58c64d46a7249cd17ca3eb6
rinsuki <428rinsuki+git@gmail.com>
2018-12-19 01:22:24 +0900

    add newline

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Hello, I am %s!</h1>", name)
+		fmt.Fprintf(w, "<h1>Hello, I am %s!</h1>\n", name)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
